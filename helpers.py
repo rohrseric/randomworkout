97c880be2b79dcc8214b0751e6ec2594f61b8888
Eric Rohrs <rohrs.eric@gmail.com>
2018-07-27 18:18:55 +0000

    login test

--- helpers.py
+++ helpers.py
@@ -27,8 +27,8 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         print(session)
-        print(session["user_id"])
         print(session.get("user_id"))
+        print(session.get("role"))
         if session.get("user_id") is None:
             return redirect("/login")
         return f(*args, **kwargs)
