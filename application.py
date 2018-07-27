b8a4477160b02ef3cadef62dee753fb7bc12b3a7
Eric Rohrs <rohrs.eric@gmail.com>
2018-07-27 18:26:44 +0000

    changed session config

--- application.py
+++ application.py
@@ -24,24 +24,24 @@ db = SQLAlchemy(app)
 #------------------------------------------
 
 # OPTION 1 ------ Configure Flask-Session
-# SESSION_TYPE = 'redis'
-# app.config.from_object(__name__)
-# Session(app)
-
-# @app.route('/set/')
-# def set():
-#     session['key'] = 'value'
-#     return 'ok'
-
-# @app.route('/get/')
-# def get():
-#     return session.get('key', 'not set')
-    
-# OPTION 2 ---- Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+SESSION_TYPE = 'redis'
+app.config.from_object(__name__)
 Session(app)
+
+@app.route('/set/')
+def set():
+    session['key'] = 'value'
+    return 'ok'
+
+@app.route('/get/')
+def get():
+    return session.get('key', 'not set')
+    
+# # OPTION 2 ---- Configure session to use filesystem (instead of signed cookies)
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
 
 
 class User(db.Model):
